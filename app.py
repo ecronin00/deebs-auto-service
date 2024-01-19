cda02be7e2fa38ab67f443da221843d0c6efbcbb
Ethan Cronin <ethancronin@ethans-mac-mini.myfiosgateway.com>
2024-01-19 12:57:53 -0500

    all pages work now

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+
+load_dotenv()
 
 app = Flask(__name__)
+#CORS(app)
+
+
 
 @app.route("/home")
 def index():
@@ -17,6 +25,14 @@ def stateInspections():
 @app.route("/brakes")
 def brakes():
     return render_template('brakes.html')
+
+@app.route("/ac-diagnosis")
+def ac_diagnosis():
+    return render_template('ac-diagnosis.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
